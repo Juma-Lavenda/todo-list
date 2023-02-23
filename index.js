97811cc94c54a74f779a1d2f8ac53eef94d33a0f
Juma-Lavenda <Lavender.dev.studyFind@gmail.com>
2023-02-22 23:47:25 -0600

    progress

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /*
 Adds a news row entry to the table when add task button is clicked
-*/
+// */
  function addRow(){
 
         var table = document.getElementById("table")
@@ -8,10 +8,23 @@ Adds a news row entry to the table when add task button is clicked
         
         var cell1 = newRow.insertCell(0);
         var cell2 = newRow.insertCell(1);
-        var cell3 = newRow.insertCell(1);
-        cell1.innerHTML = "Add a deadline";
-        cell2.innerHTML = "Add new task";
-        cell3 .innerHTML ="done";
-        // table.appendChild(newRow)
+        var cell3 = newRow.insertCell(2);
+        cell1.innerHTML = '<input id = "date" type = "date">';
+        cell1.contentEditable = true;
+        cell2.innerHTML = 'Add new task here';
+        cell2.contentEditable = true;
+        cell3 .innerHTML = '<button id = "delete" onclick = "deleteRow()">✓</button>';
+        table.appendChild(newRow)
 
-}
+ }
+
+ /**
+  * Toggle visibility of the congrats message
+  */
+function toggleCongrats(num){}
+
+/**
+ * A function that toggle visibility of row
+ */
+
+function toggleRow(){}
