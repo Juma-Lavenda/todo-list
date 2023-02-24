682e86d0987825ff0acc62ef6da546b2b0d34a58
Juma-Lavenda <Lavender.dev.studyFind@gmail.com>
2023-02-24 14:01:50 -0600

    progress

--- index.js
+++ index.js
@@ -24,20 +24,31 @@ Adds a news row entry to the table when add task button is clicked
 
 
 
-function sortTableVals(column){
+function sortTableVals(column, tag){
        
        var tableVal = document.getElementById("table");
+       const dirPath = 1;
        const table = tableVal.tBodies[0];
-       const rowVal = Array.from(table.querySelectorAll('tr')).slice(1);
+       const rowVal = Array.from(table.querySelectorAll('tr'));
        const sortedrows = rowVal.sort((a,b) => {
-              
-              const aCol = a.querySelector(`td.nth-child(${column})`).textContent.trim();
-              const bCol = b.querySelector(`td.nth-child(${column})`).textContent.trim();
-              return(aCol < bCol)
+              const aVal = a.querySelector(`td:nth-child(${column})`);
+              const bVal = b.querySelector(`td:nth-child(${column})`);
+              let aCol;
+              let bCol;
+              if (tag){
+                      aCol = aVal.querySelector('input').value;
+                      bCol = bVal.querySelector('input').value;
+              } else{
+                      aCol = aVal.textContent.trim();
+                      bCol = bVal.textContent.trim();}
+
+              return(aCol > bCol) ? 1*dirPath : -1*dirPath;
        });
-       console.log(sortedrows)
-      
-       table.appendChild(...sortedrows);
+       
+       while(table.firstChild){
+              table.removeChild(table.firstChild);
+       }
+       table.append(...sortedrows);
 }
 
 
